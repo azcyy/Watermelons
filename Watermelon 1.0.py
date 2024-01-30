969eed299b1eb0fad89c9f3005d22cb3b9f7bae9
Azcyy <154558142+azcyy@users.noreply.github.com>
2024-01-30 14:05:57 -0300

    Watermelon 1.0.py

---
+++ Watermelon 1.0.py
@@ -0,0 +1,12 @@
+# I just made this for fun  
+# I will try to upgrade this code,while I'm learning more Python
+
+ask = input('Do you like watermelons?:')
+print(ask)
+
+if ask == 'no':
+    print('UnU')
+elif ask == 'yes':
+    print('Yipee!')
+else:
+    print('error')
